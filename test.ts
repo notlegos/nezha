1cf907d661087d821edca758f62627bf9b348b63
lionkkkk <lion_yan@outlook.com>
2020-04-06 19:30:32 +0800

    no message

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-// tests go here; this will not be compiled when this package is used as a library
+input.onButtonPressed(Button.A, function () {
+    neZha.setMotorSpeed(neZha.MotorList.M1, 100)
+    neZha.setMotorSpeed(neZha.MotorList.M2, 100)
+    neZha.setMotorSpeed(neZha.MotorList.M3, 100)
+    neZha.setMotorSpeed(neZha.MotorList.M4, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    neZha.setServoAngel(neZha.ServoList.S1, 119)
+    neZha.setServoSpeed(neZha.ServoList.S2, -58)
+})
+input.onButtonPressed(Button.AB, function () {
+    neZha.stopAllMotor()
+})
+basic.forever(function () {
+	
+})
