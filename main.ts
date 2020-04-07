d47f8cdc164c99968e0596d93013567944ade439
Lionkk <Lion_yan@outlook.com>
2020-04-07 18:41:30 +0800

    第一版

--- main.ts
+++ main.ts
@@ -148,6 +148,7 @@ namespace neZha {
         iic_buffer[3] = 0;
         pins.i2cWriteBuffer(neZha_address, iic_buffer);
     }
+    
     /*
      * TODO: Setting the speed of a servo motor. 
      * @param servo A servo in the ServoList , eg: ServoList.S1
